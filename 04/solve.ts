db5b6337e75559cb463524bdf9cf0092d33f8c87
Rafał Zubala <rzubala@gmail.com>
2020-12-04 07:58:42 +0100

    day4 part2

--- 04/solve.ts
+++ 04/solve.ts
@@ -31,20 +31,65 @@ class Solve04 extends FileReader {
   }
 
   private checkPassports = () => {
-    var valid = 0
+    var required = 0, valid = 0
     this.passports.forEach(p => {
-      if (this.isValid(p)) {
-        valid++
+      if (this.isRequired(p)) {
+        required++
+        if (this.isValid(p)) {
+          valid++;
+        }
       }      
     })
-    console.log(valid)
+    console.log(required, valid)
   }
 
-  private isValid = (passport): boolean => {
+  private isRequired = (passport): boolean => {
     for (const key of this.mandatoryKeys) {
       if (!(key in passport)) {
         return false
       }
+    }
+    return true
+  }
+
+  private isValid = (passport): boolean => {
+    const byr = passport['byr']
+    if (!/([0-9]{4})/.test(byr) || +byr < 1920 || +byr > 2002) {
+      return false;
+    }
+    const iyr = passport['iyr']
+    if (!/([0-9]{4})/.test(iyr) || +iyr < 2010 || +iyr > 2020) {
+      return false;
+    }
+    const eyr = passport['eyr']
+    if (!/([0-9]{4})/.test(eyr) || +eyr < 2020 || +eyr > 2030) {
+      return false;
+    }
+    const hgt = passport['hgt']
+    const hgtregex: RegExp = /(\d+)(cm|in)/;
+    const hgtmatch: RegExpExecArray = hgtregex.exec(hgt);
+    if (hgtmatch != null) {
+      const hgtn: number = +hgtmatch[1];
+      const unit = hgtmatch[2];
+      if (unit === 'cm' && (+hgtn < 150 || +hgtn > 193)) {      
+        return false
+      } else if (unit === 'in' && (+hgtn < 59 || +hgtn > 76)) {    
+        return false
+      }
+    } else {
+      return false;
+    }
+    const hcl = passport['hcl']
+    if (hcl.length !== 7 || !/(#[0-9a-f]{6})/.test(hcl)) {
+      return false
+    }
+    const ecl = passport['ecl']
+    if (ecl.length !== 3 || !/(amb|blu|brn|gry|grn|hzl|oth)/.test(ecl)) {
+      return false
+    }
+    const pid = passport['pid']
+    if (pid.length !== 9 || !/([0-9]{9})/.test(pid)) {
+      return false
     }
     return true
   }
