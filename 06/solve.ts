c5b9bd8a66eecfbc06bc255716354b56fb26e580
Rafał Zubala <rzubala@gmail.com>
2020-12-06 07:32:28 +0100

    day6 part2

--- 06/solve.ts
+++ 06/solve.ts
@@ -6,17 +6,17 @@ class Solve06 extends FileReader {
     super();
     this.readData("input.data")
       .then((data) => {
-        this.process(data.split("\n"));
+        this.process1(data.split("\n"));
+        this.process2(data.split("\n"));
       })
       .catch((err) => console.log(err));
   }
 
-  private process = (data: string[]) => {
+  private process1 = (data: string[]) => {
     const yes: Set<string> = new Set()
     let sum = 0
     data.forEach(line => {
       if (line.trim() === '') {
-        console.log(yes)
         sum += yes.size
         yes.clear()        
       } else {
@@ -25,6 +25,40 @@ class Solve06 extends FileReader {
     })
     sum += yes.size    
     console.log('yes', sum)
+  }
+
+  private process2 = (data: string[]) => {
+    let yes = {}
+    let members = 0
+    let sum = 0    
+    data.forEach(line => {
+      if (line.trim() === '') {
+        sum += this.countYes(yes, members)
+        yes = {}
+        members = 0 
+      } else {
+        members++;
+        line.split('').forEach(y => {          
+          let cnt = yes[y]
+          if (cnt === undefined) {
+            cnt = 0
+          }
+          yes[y] = cnt + 1            
+        })          
+      }      
+    })
+    sum += this.countYes(yes, members)
+    console.log('yes', sum)
+  }
+
+  private countYes = (yes, members): number => {
+    let sum = 0
+    for (const y in yes) {
+      if (yes[y] === members) {
+        sum++
+      }
+    }
+    return sum
   }
 }
 
