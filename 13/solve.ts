f142cf981d29f6fc20fb9bbc9afad5f48f87279b
Rafał Zubala <rzubala@gmail.com>
2020-12-13 12:14:35 +0100

    efactor

--- 13/solve.ts
+++ 13/solve.ts
@@ -32,6 +32,7 @@ class Solve13 extends FileReader {
     let n = this.buses[0].id
     let timestamp = 0
     for (let bus of this.buses.slice(1)) {
+      console.log(timestamp, n, bus.id, bus.offset)
       while(true) {
         if ((timestamp + bus.offset) % bus.id === 0) {
           n *= bus.id;
