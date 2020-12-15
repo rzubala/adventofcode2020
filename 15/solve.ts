2a904e06e502a3d7927d4119891c0c11a2b6becd
Rafał Zubala <rzubala@gmail.com>
2020-12-15 08:39:48 +0100

    day15 part1

---
+++ 15/solve.ts
@@ -0,0 +1,55 @@
+import { FileReader } from "../common";
+
+class Solve15 extends FileReader {
+  private data: number[];
+
+  private init = async () => {
+    try {
+      const rawData = await this.readData("input.data");
+      this.data = rawData.split(",").map(Number);
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+    }
+  };
+
+  run = async () => {
+    await this.init();
+    this.process();
+  };
+
+  private process = () => {
+    console.log(this.data);
+    const mem = this.data.reduce((a, v, i) => {
+      a[v] = [i]
+      return a
+    }, {})
+
+    let it = this.data.length;
+    let end = 2020
+    let last = this.data[it - 1]
+    while (true) {
+      let cur
+      const indices: number[] = (mem[last] || [])
+      const len = indices.length
+      if (len < 2) {
+        cur = 0
+      } else {
+        cur = indices[len-1] - indices[len-2]
+      }
+      const newindices: number[] = (mem[cur] || [])      
+      newindices.push(it)
+      mem[cur] = newindices
+      last = cur
+      
+      console.log((it + 1), cur)
+      if ((it + 1) === end) {
+        break
+      }
+      it++
+    }
+
+  };
+}
+
+new Solve15().run();
