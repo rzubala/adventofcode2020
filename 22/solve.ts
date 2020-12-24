4394557d60e91ba3e0447b2bda6826991a82ab20
Rafał Zubala <rzubala@gmail.com>
2020-12-24 12:25:06 +0100

    day22 part2

--- 22/solve.ts
+++ 22/solve.ts
@@ -7,23 +7,23 @@ class Solve22 extends FileReader {
   private init = async () => {
     try {
       const rawData = await this.readData("input.data");
-      let first = true
+      let first = true;
       for (let line of rawData.split("\n")) {
-        if (line === '') {
-            continue
+        if (line === "") {
+          continue;
         }
-        if (line === 'Player 2:') {
-            first = false
-            continue
-        }        
-        const num = +line
+        if (line === "Player 2:") {
+          first = false;
+          continue;
+        }
+        const num = +line;
         if (isNaN(num)) {
-            continue
+          continue;
         }
         if (first) {
-            this.player1.push(num)
+          this.player1.push(num);
         } else {
-            this.player2.push(num)
+          this.player2.push(num);
         }
       }
     } catch (ex) {
@@ -34,35 +34,79 @@ class Solve22 extends FileReader {
 
   run = async () => {
     await this.init();
-    this.process();
+    this.play1();
+
+    const player1 = [...this.player1];
+    const player2 = [...this.player2];
+    this.play2(player1, player2);
+    console.log(player1.join(','))
+    console.log(player2.join(','))
+    console.log(this.score(player1), this.score(player2));
   };
 
-  private process = () => {
-    console.log(this.player1.join(', '))
-    console.log(this.player2.join(', '))
-    while (this.player1.length > 0 && this.player2.length > 0) {
-        const card1 = this.player1.shift()
-        const card2 = this.player2.shift()
-        if (card1! > card2!) {
-            this.player1.push(card1!)
-            this.player1.push(card2!)
-        } else {
-            this.player2.push(card2!)
-            this.player2.push(card1!)
-        }
+  private play2 = (player1: number[], player2: number[]): boolean => {
+    const set1 = new Set<string>()  
+    const set2 = new Set<string>()
+    while (player1.length > 0 && player2.length > 0) {      
+      const hash1 = this.hash(player1)
+      const hash2 = this.hash(player2)  
+      if (set1.has(hash1) && set2.has(hash2)) {          
+          return true
+      }
+      set1.add(hash1)
+      set2.add(hash2)
+        
+      let winner1: boolean | undefined = undefined;
+      const card1 = player1.shift();
+      const card2 = player2.shift();
+
+      if (card1! <= player1.length && card2! <= player2.length) {
+        winner1 = this.play2(player1.slice(0, card1), player2.slice(0, card2));
+      } else {
+        winner1 = card1! > card2!;
+      }
+      if (winner1) {
+        player1.push(card1!);
+        player1.push(card2!);
+      } else {
+        player2.push(card2!);
+        player2.push(card1!);
+      }
+
     }
-    console.log(this.player1.join(', '))
-    console.log(this.player2.join(', '))
-    const res1 = this.player1.reduce((a, c, i) => {
-        a += c * (this.player1.length - i)
-        return a
-    }, 0)
-    const res2 = this.player2.reduce((a, c, i) => {
-        a += c * (this.player2.length - i)
-        return a
-    }, 0)
-    console.log(res1, res2)
+    return player1.length > 0
   };
+
+  private play1 = () => {
+    const player1 = [...this.player1];
+    const player2 = [...this.player2];
+    while (player1.length > 0 && player2.length > 0) {
+      const card1 = player1.shift();
+      const card2 = player2.shift();
+      if (card1! > card2!) {
+        player1.push(card1!);
+        player1.push(card2!);
+      } else {
+        player2.push(card2!);
+        player2.push(card1!);
+      }
+    }
+    console.log(this.score(player1), this.score(player2));
+  };
+
+  private score = (player: number[]): number => {
+    return player.reduce((a, c, i) => {
+      a += c * (player.length - i);
+      return a;
+    }, 0);
+  };
+
+  private hash = (player: number[]): string => {
+      return player.reduce((a,v) => {
+        a += v + ","
+        return a
+      }, "")
+  }
 }
 
 new Solve22().run();
