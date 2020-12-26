846fed1b139def2708bf4fc2b8d365307fda0cbd
Rafał Zubala <rzubala@gmail.com>
2020-12-26 20:39:42 +0100

    day23 part2

--- 23/solve.ts
+++ 23/solve.ts
@@ -51,7 +51,7 @@ const buffer = (data: number[], min: number, max: number) => {
 }
 
 class Solve23 {
-  public process1a = () => {
+  public process1 = () => {
     const input = "614752839";
     const buf = buffer(input.split('').map(e => +e), 1, 9)
     let currentCup = +input[0]
@@ -66,21 +66,21 @@ class Solve23 {
     console.log(buf.result())
   };
 
-  public process1b = () => {
+  public process2 = () => {
     const input = "614752839";
-    const data = input.split('').map(e => +e)
+    const data = [...input.split('').map(e => +e), ...Array.from({length: 1000000 - input.length}, (v,k) => 10 + k)]
     let currentCup = data[0]
     const len = data.length
-    const min = Math.min(...data)
-    const max = Math.max(...data)
-    const buffer = new Array<number>(len + 1)
+    const min = 1
+    const max = 1000000
+    const buffer = new Array<number>(len + 1)    
     const group3 = new Array<number>(3)
     for (let i=0;i<len-1;i++) {
       buffer[data[i]] = data[i+1]    
     }
     buffer[data[len-1]] = data[0]
 
-    for (let i=0;i<100;i++) {
+    for (let i=0;i<10000000;i++) {
       const firstRemoved = buffer[currentCup]
       let toRemove = group3[0] = firstRemoved
       for (let i=1;i<3;i++) {
@@ -104,41 +104,13 @@ class Solve23 {
       buffer[group3[2]] = afterDestination
       currentCup = buffer[currentCup]
     }
-    let score = ''
-    let iter = buffer[1]
-    while(score.length < len - 1) {
-      score += iter
-      iter = buffer[iter]
-    }
-    console.log(score)
-  }
 
-  public process2 = () => {
-    const input = "389125467"
-    const bufferData = [...input.split('').map(e => +e), ...Array.from({length: 1000000 - input.length}, (v,k) => 10 + k)]
-    const buf = buffer(bufferData, 1, 1000000)
-    
-    // console.log(buffer.length)
-    // for (let i=0;i<15;i++) {
-    //   console.log(buffer[i])
-    // }
-    // console.log(buffer[buffer.length-1])
-    //moves 10000000
-    //last value 1000000
+    const item1 = buffer[1]
+    const item2 = buffer[item1]
+    console.log(item1*item2)
 
-
-    let currentCup = +input[0]
-    let currentCupIndex = 0
-    //for (let i=0;i<10000000;i++) {      
-    for (let i=0;i<15;i++) {
-      const removed = buf.pop3(currentCupIndex)      
-      const [destination, index] = buf.destinationCup(currentCup)      
-      buf.insert(removed, index)           
-      currentCupIndex = buf.indexOf(currentCup) + 1
-      currentCup = buf.get(currentCupIndex)
-    }    
   }
 }
 
-new Solve23().process1a();
-new Solve23().process1b();
+new Solve23().process1();
+new Solve23().process2();
